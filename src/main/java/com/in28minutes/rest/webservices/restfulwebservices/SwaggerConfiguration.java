e207df309b0d57b1052d194c50d3b50d8430c171
Francisco Javier Garcia Orduna <francisco.orduna@king.com>
2018-12-05 11:26:57 +0100

    step 22

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfiguration.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/SwaggerConfiguration.java
@@ -2,6 +2,14 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,8 +18,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfiguration {
 
+    public static final Contact DEFAULT_CONTACT = new Contact("Javier Garcia", "http://www.fjgarciao.com", "fjgarciao@gmail.com");
+    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title", "Awesome API Description", "1.0", "urn:tos",
+                                                      DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
+    private static final Set<String> DEFAULT_PRODUCERS_AND_CONSUMERS = new HashSet<>(Arrays.asList(MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE));
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(DEFAULT_API_INFO)
+            .produces(DEFAULT_PRODUCERS_AND_CONSUMERS)
+            .consumes(DEFAULT_PRODUCERS_AND_CONSUMERS);
     }
 }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -3,15 +3,20 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel(description = "All details about the user.")
 public class User {
 
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes = "Name should have at least 2 characters")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Birthdate should be in the past")
     private Date birthDate;
 
     protected User() {
