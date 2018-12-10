4495093312dba7b851afc9e14d7ae40c3851c694
Francisco Javier Garcia Orduna <francisco.orduna@king.com>
2018-12-10 16:29:07 +0100

    step 27

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonVersioningProvider.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonVersioningProvider.java
@@ -15,4 +15,34 @@ public class PersonVersioningProvider {
     public PersonV2 retrievePersonV2() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+    @GetMapping(value = "/person/param", params = "version=1")
+    public PersonV1 retrievePersonParamV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/param", params = "version=2")
+    public PersonV2 retrievePersonParamV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 retrievePersonHeaderV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 retrievePersonHeaderV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 retrievePersonProducesV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 retrievePersonProducesV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
 }
